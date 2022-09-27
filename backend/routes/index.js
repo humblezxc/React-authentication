64d7d086452fec820d4d0142fdfc442c2767c1aa
humblezxc <you@example.com>
2022-09-28 00:40:35 +0200

    updated dir routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { Login } from '../controllers/Users.js'
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
 
 const routes = express.Router();
 
+router.get('/users', verifyToken, getUsers);
 routes.post('/login', Login);
+router.get('/token', refreshToken);
 
