8daba596a47c5eee00c71df25e3f9627c2f0f4a7
humblezxc <you@example.com>
2022-09-27 22:02:57 +0200

    Created Register form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
+
 
 
 function App() {
     return (
         <BrowserRouter>
+            <Switch>
                 <Route exact path="/">
                     <Login/>
                 </Route>
+                <Route exact path="/register">
+                    <Register/>
+                </Route>
+            </Switch>
         </BrowserRouter>
     );
 }
