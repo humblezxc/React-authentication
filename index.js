5b772577d68929f16c673b323798dcab8f877bd2
humblezxc <you@example.com>
2022-09-28 00:46:01 +0200

    updated index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import cors from 'cors'
-const app = express();
-
-app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
-app.listen(5000, () => console.log('Server running at port 5000'))
