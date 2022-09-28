370bcfeb65cc6cf0c3a3b063ea9a688c7150a8f7
humblezxc <you@example.com>
2022-09-28 11:27:56 +0200

    Updated index files

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors'
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import router from './routes/index.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
 dotenv.config();
-
 const app = express();
 
 app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { Login } from '../controllers/Users.js'
+import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 
-const routes = express.Router();
+const router = express.Router();
 
 router.get('/users', verifyToken, getUsers);
-routes.post('/login', Login);
+router.post('/users', Register);
+router.post('/login', Login);
 router.get('/token', refreshToken);
+router.delete('/logout', Logout);
 
+export default router;
