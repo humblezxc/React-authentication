645575eef23bef44961ddb6f2f7e387caa037283
humblezxc <you@example.com>
2022-09-29 21:48:49 +0200

    Change  package

--- backend/middleware/VerifyToken.js
+++ backend/middleware/VerifyToken.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const verifyToken = (req, res, next) => {
+    console.log(process.env.ACCESS_TOKEN_SECRET)
+    console.log(req.headers)
+
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null) return res.sendStatus(401);
-    console.log(process.env.ACCESS_TOKEN_SECRET)
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err) return res.sendStatus(403);
         req.email = decoded.email;
