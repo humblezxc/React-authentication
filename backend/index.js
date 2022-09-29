b24c28bb250c437d6e1565124e422e93a2238b57
humblezxc <you@example.com>
2022-09-29 20:17:03 +0200

    Change  package

--- backend/index.js
+++ backend/index.js
@@ -22,11 +22,11 @@ console.log(path.join(__dirname, '/dist', 'index.html'));
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('frontend/build'));
+    app.use(express.static(path.join(__dirname, 'build')));
 
-    // Express serve up index.html file if it doesn't recognize route
-    app.get('*', (req, res) => {
-        res.sendFile("/app/frontend/build/index.html");
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
 
