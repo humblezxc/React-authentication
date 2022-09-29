8f8213b14631590bf0c7f021baeb09f8f70b1a71
humblezxc <you@example.com>
2022-09-29 22:13:09 +0200

    Change  package

--- backend/controllers/RefreshToken.js
+++ backend/controllers/RefreshToken.js
@@ -8,30 +8,30 @@ export const refreshToken = async(req, res) => {
     console.log(process.env.REFRESH_TOKEN_SECRET)
     console.log(req.cookies)
 
-    try {
-        const refreshToken = req.cookies.refreshToken;
-        if(!refreshToken) return res.sendStatus(401);
-        const user = await Users.findAll({
-            where:{
-                refresh_token: refreshToken
-            }
-        });
-        if(!user[0]) return res.sendStatus(403);
-            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-            if(err) return res.sendStatus(403);
-            const userId = user[0].id;
-            const name = user[0].name;
-            const email = user[0].email;
-            const accessToken = jwt.sign(
-                {userId, name, email},
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '15s' }
-            );
-            res.json({ accessToken });
-        });
-    } catch (error) {
+    // try {
+    //     const refreshToken = req.cookies.refreshToken;
+    //     if(!refreshToken) return res.sendStatus(401);
+    //     const user = await Users.findAll({
+    //         where:{
+    //             refresh_token: refreshToken
+    //         }
+    //     });
+    //     if(!user[0]) return res.sendStatus(403);
+    //         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    //         if(err) return res.sendStatus(403);
+    //         const userId = user[0].id;
+    //         const name = user[0].name;
+    //         const email = user[0].email;
+    //         const accessToken = jwt.sign(
+    //             {userId, name, email},
+    //             process.env.ACCESS_TOKEN_SECRET,
+    //             { expiresIn: '15s' }
+    //         );
+    //         res.json({ accessToken });
+    //     });
+    // } catch (error) {
         console.log(process.env.ACCESS_TOKEN_SECRET)
         console.log(process.env.REFRESH_TOKEN_SECRET)
-        console.log(error);
-    }
+        // console.log(error);
+    // }
 }
