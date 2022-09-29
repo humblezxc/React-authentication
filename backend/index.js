c3383be8a42dadf5cb68f4d4f02e1871c9975be7
humblezxc <you@example.com>
2022-09-29 21:43:00 +0200

    Change  package

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,8 @@ const __dirname = path.dirname(__filename);
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
-
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
 
