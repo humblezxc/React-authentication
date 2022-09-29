4e308a8e82eaa0ea2113eaff7de3f1781a23094f
humblezxc <you@example.com>
2022-09-29 22:48:02 +0200

    Change  package

--- backend/index.js
+++ backend/index.js
@@ -12,16 +12,16 @@ import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(router);
 
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
 
 app.use(cors({ credentials:true, origin:'*' }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(router);
 
 app.listen(process.env.PORT || 5000, ()=> console.log(`Server running at port ${process.env.PORT}`));
